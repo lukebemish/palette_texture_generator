e480d00db4eb7893ffa3dd14e1f0f800f8887783
Luke Bemish <lukebemish@gmail.com>
2022-02-05 17:37:32 -0600

    Add debugging options, ability to "extend" small palettes

--- palette_texture_generator.py
+++ palette_texture_generator.py
@@ -3,7 +3,14 @@ from PIL import Image
 import os
 import sys
 
-def gen_from_name(name):
+def is_color_present(color, palette):
+    for i in palette:
+        diff = [abs(color[j] - i[j]) for j in range(len(color))]
+        if all([c < 5 for c in diff]):
+            return True
+    return False
+
+def gen_from_name(name, debug, extend):
     overlay = "source_textures/"+name+"_overlay.png"
     paletted = "source_textures/"+name+"_paletted.png"
     overlay_img = Image.open(overlay).convert('RGBA')
@@ -22,10 +29,20 @@ def gen_from_name(name):
             for i in range(16):
                 for j in range(16):
                     if b_arr[i,j,3] == 255:
-                        if not b_arr[i,j].tolist() in palette:
+                        if not is_color_present(b_arr[i,j].tolist(), palette):
                             palette.append(b_arr[i,j].tolist())
             palette.sort(key=lambda x:sum(x))
             out_arr = np.zeros((16,16,4),dtype=np.uint8)
+            #palette size debugging
+            if debug:
+                print(background[0:-4]+" palette size: "+str(len(palette)))
+            if (len(palette) < 6 and extend):
+                #add a palette value on the top and bottom side
+                #try a .1 alpha?
+                high = [i * .9  + 255 * .1 for i in palette[-1]]
+                low = [i * .9 for i in palette[0]]
+                palette.append(high)
+                palette.insert(0, low)
             for i in range(16):
                 for j in range(16):
                     out = b_arr[i,j].astype(np.float64)
@@ -40,11 +57,13 @@ def gen_from_name(name):
             out_img.save(out_file)
 
 if __name__ == "__main__":
+    debug = "--debug" in sys.argv
+    extend = "--extend" in sys.argv
     arg = sys.argv[1]
     if arg=="--all":
         files = [i[0:-12] for i in os.listdir("source_textures") if i.endswith("_overlay.png")]
         for f in files:
             if os.path.exists("source_textures/"+f+"_paletted.png"):
-                gen_from_name(f)
+                gen_from_name(f, debug, extend)
     else:
-        gen_from_name(sys.argv[1])
+        gen_from_name(sys.argv[1], debug, extend)
